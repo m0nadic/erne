984fc24224cc514bf295827f43c2b5438ed38292
m0nadic <anirbang.m0@gmail.com>
2020-10-18 01:23:23 +0530

    multiple records

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/brianvoe/gofakeit/v5"
 	"os"
@@ -9,11 +10,24 @@ import (
 )
 
 func main() {
-	gofakeit.Seed(0)
+	count := flag.Int("n", 1, "number of records to generate")
 
+	flag.Parse()
+
+	gofakeit.Seed(0)
+	for i := 0; i < *count; i++ {
+		generateRecord(os.Args[1:])
+	}
+}
+
+func generateRecord(args []string) {
 	m := make(map[string]string)
-	for _, a := range os.Args[1:] {
+	for _, a := range args {
+
 		xs := strings.Split(a, "=")
+		if len(xs) != 2 {
+			continue
+		}
 		fn := genMap[xs[1]]
 		m[xs[0]] = fn()
 	}
